20e19935280b9619610618fb2acf93ef150109f7
mxdjava <charismaticdrugger@gmail.com>
2023-06-28 19:15:41 +0600

    Прописаны все требуемые условия задачи, но не проверена работа метода getUserByCarModelAndSeries Не подчищен код с закомментированными строками Добавил CarService

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -17,13 +17,18 @@ public class MainApp {
       UserService userService = context.getBean(UserService.class);
       User user1 = new User("User1", "Lastname1", "user1@mail.ru");
       Car car1 = new Car("Car1 Model", 123);
+
       user1.setCar(car1);
+      userService.add(user1);
       User user2 = new User("User2", "Lastname2", "user2@mail.ru");
       Car car2 = new Car("Car2 Model", 234);
       user2.setCar(car2);
+      userService.add(user2);
       User user3 = new User("User3", "Lastname3", "user3@mail.ru");
       Car car3 = new Car("Car3 Model", 345);
       user3.setCar(car3);
+      userService.add(user3);
+
 //      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
 //      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
 //      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/CarServiceImpl.java
@@ -0,0 +1,19 @@
+package hiber.service;
+
+import hiber.model.Car;
+import hiber.model.User;
+
+import java.util.List;
+
+public class CarServiceImpl implements CarService {
+
+    @Override
+    public void add(Car car) {
+
+    }
+
+    @Override
+    public List<User> listCars() {
+        return null;
+    }
+}
